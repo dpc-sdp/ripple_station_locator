bc92401c7b49e3bf0f3c8867668d339a24f0b2ae
Nick Georgiou <nickgeorgi@gmail.com>
2023-05-05 15:35:31 +1000

    Police station locator

---
+++ index.vue
@@ -0,0 +1,19 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'RplStationLocator',
+  props: {
+  },
+  components: {},
+  data: function () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "~@dpc-sdp/ripple-global/scss/settings";
+  @import "~@dpc-sdp/ripple-global/scss/tools";
+</style>
