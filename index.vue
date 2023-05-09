6e90f7d544bb2f7d0f0963975b531970e4e13d74
Nick Georgiou <nickgeorgi@gmail.com>
2023-05-09 16:03:30 +1000

    Add tabs and map icon.

--- index.vue
+++ index.vue
@@ -1,14 +1,52 @@
 <template>
+  <div class="vp-station-locator">
+    <rpl-tabs class="vp-station-locator__tabs" :tabs="tabs" :activeTab="activeTab" @rpl-tab-switch="switchTab" />
+    <div class="vp-station-locator__map" v-if="activeTab === 'map'">Map view</div>
+    <div class="vp-station-locator__list" v-if="activeTab === 'list'">List view</div>
+  </div>
 </template>
 
 <script>
+import { RplSearchResultsLayout } from '@dpc-sdp/ripple-search'
+import RplTabs from '@dpc-sdp/ripple-tabs/Tabs.vue'
+
 export default {
   name: 'RplStationLocator',
   props: {
   },
-  components: {},
-  data: function () {
-    return {}
+  components: {
+    RplSearchResultsLayout,
+    RplTabs
+  },
+  data () {
+    return {
+      activeTab: 'list',
+      tabs: [
+        {
+          label: 'List view',
+          key: 'list',
+          icon: 'list'
+        },
+        {
+          label: 'Map view',
+          key: 'map',
+          icon: 'map_marker'
+        }
+      ],
+    }
+  },
+  methods: {
+    handleEvent (eventName, value) {
+    },
+    switchTab (tab) {
+      this.activeTab = tab
+      this.handleEvent('tab-change', tab)
+      // if (tab === 'map') {
+      //   this.$nextTick(() => {
+      //     this.mapInstance.updateSize()
+      //   })
+      // }
+    },
   }
 }
 </script>
